32773f4f7aa4bf818ed41b032f8c778f63c4e83b
YaroslavYedyn <clavik.edin@gmail.com>
2021-06-15 11:36:57 +0300

    regexp fix,sort

--- src/Components/InfluencerDetails/InfluencerDetails.js
+++ src/Components/InfluencerDetails/InfluencerDetails.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-	useParams
+    useParams
 } from "react-router-dom";
 
 import arrowUp from '../Items/Icons/arrow-up.svg'
@@ -21,121 +21,120 @@ import Profiles from '../Items/Profiles/Profiles'
 // import photoDefault from '../Items/Icons/vector.svg';
 
 const InfluencerDetails = () => {
-	const params = useParams();
-	if (!params[routes.INFLUENCER_ID]) {
-		window.location.href = configFront.URL + `${routes.INFLUENCERS}`
-	}
-	const [isLoading, setIsLoading] = useState(false)
+    const params = useParams();
+    if (!params[routes.INFLUENCER_ID]) {
+        window.location.href = configFront.URL + `${routes.INFLUENCERS}`
+    }
+    const [isLoading, setIsLoading] = useState(false)
 
-	const [values, setValues] = useState({});
-	const [initial, setInitial] = useState(true);
+    const [values, setValues] = useState({});
+    const [initial, setInitial] = useState(true);
 
-	useEffect(() => {
-			async function Start() {
-				setIsLoading(true)
-				setInitial(false)
-				const initialState = await InfluencersService.getSingleInfluencer(params[routes.INFLUENCER_ID])
+    useEffect(() => {
+            async function Start() {
+                setIsLoading(true)
+                setInitial(false)
+                const initialState = await InfluencersService.getSingleInfluencer(params[routes.INFLUENCER_ID])
 
-				console.log(initialState);
+                console.log(initialState);
 
-				const arr = initialState.social_profiles;
-				arr.splice(1, 3).reverse().forEach(value => arr.push(value))
-				arr.splice(1, 2).reverse().forEach(value => arr.push(value))
-
-
-				if (initialState.social_profiles.length) {
-					initialState.social_profiles.forEach((value) => {
-						let newValue = value.social_network_followers.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-						value.social_network_followers = newValue;
-						console.log(values)
-					})
-					initialState.photos = [...initialState.instagram_photos, ...initialState.youtube_videos]
-
-					initialState.photos=initialState.photos.sort((a, b) => a.publishedAt - b.publishedAt)
-
-				}
-
-				if (!initialState.birthdate) {
-					setValues({...initialState})
-
-				} else {
-					const newDate = initialState.birthdate.split('T').shift();
-					setValues({...initialState, birthdate: newDate.split('-').reverse().join('.')});
-				}
-				setIsLoading(false)
-			}
-
-			if (initial) {
-				Start()
-			}
-		}
-	)
-
-	return (
-		<div className={classes.mainContainer}>
-			<Sidebar/>
-			<UsersListHeader titleHeader={classes.titleUsers}
-							 title={isLoading ? '' : values.first_name && values.last_name ? `${values.first_name} ${values.last_name}` : ''}
-							 titleBtn='Create New'
-							 upArrow={arrowUp}
-							 btnHeader={classes.btnHeader}
-							 titleBtnEdit={'Edit'}
-							 EditInf={classes.EditInf}
-							 item={'influencer'}
-							 icon={{background: `url(${edit}) no-repeat`, backgroundPosition: 'left 8px top 8px'}}
-			/>
-			{isLoading ?
-				<Loading message='Please, wait...'/> :
-				<div className={classes.infoContainer}>
-					<div className={classes.rightBox}>
-						<img
-							src={values.profile_picture ? values.profile_picture : defaultImg}
-							alt="avatar"
-						/>
-					</div>
-					<section className={classes.leftBox}>
-						<div
-							className={classes.FullName}>{values.first_name && values.last_name ? `${values.first_name} ${values.last_name}` : ''}</div>
-						<div className={classes.infoDetails}>
-							<div>
-								<span className={classes.positionBirthday}>Birthday:</span>
-								<span className={classes.textDetail}>{values.birthdate}</span>
-							</div>
-
-							<div className={classes.Occupation}>
-								<span className={classes.positionDetail}>Occupation: </span>
-								<span className={classes.textDetail}> {values.profession} </span>
-							</div>
-
-							<div className={classes.mainSocialContainer}>
-
-								{values.social_profiles && values.social_profiles.length ? values.social_profiles.map((soc, index) =>
-									<div key={index}>
-										<Profiles network_name={soc.social_network_name}
-												  followers={soc.social_network_followers}
-												  profile={soc.social_network_profile}/>
-									</div>
-								) : ''
-								}
-
-							</div>
-						</div>
-					</section>
-				</div>
-			}
-			{isLoading ? "" :
-				<section>
-					<div className={classes.ContainerPhoto}>
-
-						{values.photos ? values.photos.map((photo) => <div
-						    className={classes.instPhoto}><img src={photo.preview} alt={'instPhoto'}/></div>) : ''}
+                const arr = initialState.social_profiles;
+                arr.splice(1, 3).reverse().forEach(value => arr.push(value))
+                arr.splice(1, 2).reverse().forEach(value => arr.push(value))
 
 
+                if (initialState.social_profiles.length) {
+                    initialState.social_profiles.forEach((value) => {
+                        let newValue = value.social_network_followers.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+                        value.social_network_followers = newValue;
+                        console.log(values)
+                    })
+                    initialState.photos = [...initialState.instagram_photos, ...initialState.youtube_videos]
 
-					</div>
-				</section>}
-		</div>
-	)
+                    initialState.photos = initialState.photos.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+
+                }
+
+                if (!initialState.birthdate) {
+                    setValues({ ...initialState })
+
+                } else {
+                    const newDate = initialState.birthdate.split('T').shift();
+                    setValues({ ...initialState, birthdate: newDate.split('-').reverse().join('.') });
+                }
+                setIsLoading(false)
+            }
+
+            if (initial) {
+                Start()
+            }
+        }
+    )
+
+    return (
+        <div className={classes.mainContainer}>
+            <Sidebar/>
+            <UsersListHeader titleHeader={classes.titleUsers}
+                             title={isLoading ? '' : values.first_name && values.last_name ? `${values.first_name} ${values.last_name}` : ''}
+                             titleBtn='Create New'
+                             upArrow={arrowUp}
+                             btnHeader={classes.btnHeader}
+                             titleBtnEdit={'Edit'}
+                             EditInf={classes.EditInf}
+                             item={'influencer'}
+                             icon={{ background: `url(${edit}) no-repeat`, backgroundPosition: 'left 8px top 8px' }}
+            />
+            {isLoading ?
+                <Loading message='Please, wait...'/> :
+                <div className={classes.infoContainer}>
+                    <div className={classes.rightBox}>
+                        <img
+                            src={values.profile_picture ? values.profile_picture : defaultImg}
+                            alt="avatar"
+                        />
+                    </div>
+                    <section className={classes.leftBox}>
+                        <div
+                            className={classes.FullName}>{values.first_name && values.last_name ? `${values.first_name} ${values.last_name}` : ''}</div>
+                        <div className={classes.infoDetails}>
+                            <div>
+                                <span className={classes.positionBirthday}>Birthday:</span>
+                                <span className={classes.textDetail}>{values.birthdate}</span>
+                            </div>
+
+                            <div className={classes.Occupation}>
+                                <span className={classes.positionDetail}>Occupation: </span>
+                                <span className={classes.textDetail}> {values.profession} </span>
+                            </div>
+
+                            <div className={classes.mainSocialContainer}>
+
+                                {values.social_profiles && values.social_profiles.length ? values.social_profiles.map((soc, index) =>
+                                    <div key={index}>
+                                        <Profiles network_name={soc.social_network_name}
+                                                  followers={soc.social_network_followers}
+                                                  profile={soc.social_network_profile}/>
+                                    </div>
+                                ) : ''
+                                }
+
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            }
+            {isLoading ? "" :
+                <section>
+                    <div className={classes.ContainerPhoto}>
+
+                        {values.photos ? values.photos.map((photo) => <div
+                            className={classes.instPhoto}><img src={photo.preview} alt={'instPhoto'}/></div>) : ''}
+
+
+                    </div>
+                </section>}
+        </div>
+    )
 }
 
 export default InfluencerDetails;
--- src/Constants/regexp.enum.js
+++ src/Constants/regexp.enum.js
@@ -1,8 +1,8 @@
 module.exports = {
-	EMAIL_REGEXP: /^[\w.-]+@[a-zA-Z]+\.[a-zA-Z]+$/,
-	FIRST_LAST_NAME_REGEXP: /^[a-zA-Z ,.'-]{2,50}$/,
-	PASSWORD_REGEXP: /^(?=.*[A-Za-z])(?=.*\d)([A-Za-z\d@$!%*#?&]?){4,50}/,
-	PHONE_REGEXP: /^[\d+()-]{7,15}$/,
-	FOLLOWERS_REGEXP: /^[1-9]+[\d]{0,14}$/,
-	PROFILE_REGEXP: /^[a-zA-Z .-@_/:]{2,50}$/,
+    EMAIL_REGEXP: /^[\w.-]+@[a-zA-Z]+\.[a-zA-Z]+$/,
+    FIRST_LAST_NAME_REGEXP: /^[a-zA-Z ,.'-]{2,50}$/,
+    PASSWORD_REGEXP: /^(?=.*[A-Za-z])(?=.*\d)([A-Za-z\d@$!%*#?&]?){4,50}/,
+    PHONE_REGEXP: /^[\d+()-]{7,15}$/,
+    FOLLOWERS_REGEXP: /^[1-9]+[\d]{0,14}$/,
+    PROFILE_REGEXP: /^[a-zA-Z .-@_/:]{2,100}$/,
 };
